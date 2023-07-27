9795b74f12dd58582e06b7177adea2af50b9c1bb
ESAEM-Emmanuel <emmanuelessimagel@gmail.com>
2023-07-27 13:00:09 +0100

    cuntenerisation

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Utilisation de l'image de base officielle Python
+FROM python:3.9-slim
+
+# Définir le répertoire de travail
+WORKDIR /app
+
+# Copier les fichiers de dépendances dans le conteneur
+COPY requirements.txt .
+
+# Installer les dépendances
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copier l'ensemble de l'application dans le conteneur
+COPY . .
+
+# Définir la commande par défaut pour exécuter l'application FastAPI
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.middleware.wsgi import WSGIMiddleware
+# from fastapi.middleware.wsgi import WSGIMiddleware
 
 app = FastAPI()
-wsgi_app = WSGIMiddleware(app)
+# wsgi_app = WSGIMiddleware(app)
 
 
 origins = ["*"]
@@ -23,6 +23,13 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
     return {"item_id": item_id}
+
+# Définir une route POST avec des paramètres
+@app.post("/users/{user_id}")
+async def create_user(user_id: int, name: str):
+    # Logique pour créer l'utilisateur avec l'ID et le nom fournis
+    
+    return {"message": f"Utilisateur créé avec l'ID {user_id} et le nom {name}"}
 
 # Exécutez l'application avec uvicorn
 if __name__ == "__main__":
